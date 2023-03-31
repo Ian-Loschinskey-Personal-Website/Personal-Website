5f6a4c9b9be5c04f391a65ee7b578137ad071373
Ian <ian.loschinskey@gmmail.com>
2023-03-30 20:47:07 -0400

    Changes progress

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# Personal-Website
+# This Website shows my FrontEnd Development work and skills as I learn more everyday and continue my growth.
+
+
+# Personal Site
+
+## 3 Pages: Home, About, Porfolio
+## Description: This pages show information about me.
+## How to install/run: This website can be seen in my public Github link located below page.
+# Can be pulled if want so in vscode and It is live with github.
+## Credits/contributers
+# Creator: Ian Loschinskey
+## Badges:N/A
+## LicenseN/A
---
+++ javascripts.js
@@ -0,0 +1,65 @@
+if (window.location.href.indexOf("about.html") > -1) {
+    window.addEventListener('load', function() {
+      alert('Click "Reveal" to show hidden!');
+    });
+  }
+//
+if (window.location.href.indexOf("index.html") > -1) {
+    window.addEventListener('load', function() {
+        alert('My home page.')
+});
+}
+//
+//
+const button = document.getElementById('button1-js'); // Fetches the ID from <button> in first section. 
+const hiddenPar = document.getElementById('about1'); // Fetches the ID from <p> in first section.
+const img1 = document.getElementById('sect1-img');  // Fetches the ID from <img> in first section.
+const read1 = document.getElementById('about-read') // Fetches the ID from <h1> in first section.
+
+// function for "Reveal"
+button.addEventListener("click", () => { // when "Reveal" is click It'll show
+    img1.style.display = "block"        // the id thats style for "display: none;"
+    hiddenPar.style.display = "block"
+    read1.style.display = "block"
+    button.style.display = "none"
+
+});
+//
+//
+//
+const button2 = document.getElementById('button2-js'); // Fetches the ID from <button> in first section. 
+const hiddenPar2 = document.getElementById('about2'); // Fetches the ID from <p> in first section.
+const img2 = document.getElementById('sect2-img');  // Fetches the ID from <img> in first section.
+const read2 = document.getElementById('about-gaming') // Fetches the ID from <h1> in first section.
+
+// function for "Reveal"
+button2.addEventListener("click", () => { // when "Reveal" is click It'll show
+    img2.style.display = "block"        // the id thats style for "display: none;"
+    hiddenPar2.style.display = "block"
+    read2.style.display = "block"
+    button2.style.display = "none"
+
+});
+//
+//
+//
+const button3 = document.getElementById('button3-js'); // Fetches the ID from <button> in first section. 
+const hiddenPar3 = document.getElementById('about3'); // Fetches the ID from <p> in first section.
+const img3 = document.getElementById('sect3-img');  // Fetches the ID from <img> in first section.
+const read3 = document.getElementById('about-anime') // Fetches the ID from <h1> in first section.
+const final = document.getElementById('about-h1-m2') // Fetches the ID on the last <p> in about.html
+
+// function for "Reveal"
+button3.addEventListener("click", () => { // when "Reveal" is click It'll show
+    img3.style.display = "block"        // the id thats style for "display: none;"
+    hiddenPar3.style.display = "block"
+    read3.style.display = "block"
+    final.style.display = "block"
+    button3.style.display = "none"
+});
+// Here my "Hello world" tier Javascript function, loaded via an external file
+// e.g., a console.log of a given string
+function Hello() {
+    console.log("Hello world!");
+  }
+  
