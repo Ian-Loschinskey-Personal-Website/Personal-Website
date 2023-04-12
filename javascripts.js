1d01f62334f7dddc1f319faee83b3423e4191a3b
Ian <ian.loschinskey@gmmail.com>
2023-04-12 19:26:18 -0400

    Hotfix on form also added new .js for contact.html

--- javascripts.js
+++ javascripts.js
@@ -62,24 +62,3 @@ button3.addEventListener("click", () => { // when "Reveal" is click It'll show
 function Hello() {
     console.log("Hello world!");
   }
-  // Form of Subsmission
-  const form = document.querySelector("#form")
-  const submitButton = document.querySelector("#submit")
-  const scriptURL = 'https://httpbin.org/post'
-
-  form.addEventListener('submit', e => {
-    submitButton.disabled = true
-    e.preventDefault()
-    let requestBody = new FormData(form)
-    fetch(scriptURL, { method: 'POST', body: requestBody})
-      .then(response => {
-         alert('Success!', response)
-         submitButton.disabled = false
-        })
-      .catch(error => {
-      alert('Error!', error.message)
-        submitButton.disabled = false
-
-      }
-      )
-  })
